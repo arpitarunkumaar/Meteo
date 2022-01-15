ba79bbc57985dc233e8e87359a9325a966379583
Arpit Arun Kumaar <arpitak14@outlook.com>
2022-01-14 20:47:09 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Arpit Arun Kumaar on 2022-01-14.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
